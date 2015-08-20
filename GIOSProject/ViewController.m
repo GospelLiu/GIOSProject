4f00971035c5ef464cd946c3cdafb64b0b224e12
liufuyin <fuyin.lfy@alibaba-inc.com>
2015-08-20 16:03:56 +0800

    test...

--- GIOSProject/ViewController.m
+++ GIOSProject/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"gosepl test ...");
 }
 
 - (void)didReceiveMemoryWarning {
